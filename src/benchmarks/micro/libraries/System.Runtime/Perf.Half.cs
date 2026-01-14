c1b3a14b9bf75c83c8905a2aeea1eaa6ab740774
Canino, Anthony <anthony.canino@intel.com>
2026-01-14 12:15:23 -0800

    Half edits.

--- src/benchmarks/micro/libraries/System.Runtime/Perf.Half.cs
+++ src/benchmarks/micro/libraries/System.Runtime/Perf.Half.cs
@@ -17,6 +17,8 @@ namespace System.Tests
     [BenchmarkCategory(Categories.Libraries)]
     public class Perf_Half
     {
+        private const int LoopCount = 1000;
+
         public static IEnumerable<Half> Values => new Half[]
         {
             BitConverter.UInt16BitsToHalf(0x03ff),  //Maximum subnormal number in Half
@@ -32,63 +34,167 @@ namespace System.Tests
             float.NaN
         };
 
-        public static IEnumerable<Half[]> Values => new Half[]
+        public static IEnumerable<object[]> DoubleValues() 
         {
-            { BitConverter.UInt16BitsToHalf(0x03ff), BitConverter.UInt16BitsToHalf(0x03ff) },  //Maximum subnormal number in Half
-            { (Half)12345.0f, (Half)12345.0f }, /* same value used by other tests to compare the perf */
-            { BitConverter.UInt16BitsToHalf(0x7dff), BitConverter.UInt16BitsToHalf(0x7dff) }   //NaN
+            yield return new object[] { BitConverter.UInt16BitsToHalf(0x03ff), BitConverter.UInt16BitsToHalf(0x03ff) };  //Maximum subnormal number in Half;
+            yield return new object[] { (Half)12345.0f, (Half)12345.0f }; /* same value used by other tests to compare the perf */
+            yield return new object[] { BitConverter.UInt16BitsToHalf(0x7dff), BitConverter.UInt16BitsToHalf(0x7dff) };   //NaN
         }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(SingleValues))]
-        public Half SingleToHalf(float value) => (Half)value;
+        public Half SingleToHalf(float value)
+        {
+            Half result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = (Half)value;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public float HalfToSingle(Half value) => (float)value;
+        public float HalfToSingle(Half value)
+        {
+            float result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = (float)value;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public Half HalfAddition(Half left, Half right) => left + right;
+        public Half HalfAddition(Half left, Half right)
+        {
+            Half result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left + right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public Half HalfSubtraction(Half left, Half right) => left - right;
+        public Half HalfSubtraction(Half left, Half right)
+        {
+            Half result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left - right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public Half HalfMultiplication(Half left, Half right) => left * right;
+        public Half HalfMultiplication(Half left, Half right)
+        {
+            Half result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left * right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public Half HalfDivision(Half left, Half right) => left / right;
+        public Half HalfDivision(Half left, Half right)
+        {
+            Half result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left / right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public Half HalfNegation(Half value) => -value;
+        public Half HalfNegation(Half value)
+        {
+            Half result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = -value;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public bool HalfEquals(Half left, Half right) => left == right;
+        public bool HalfEquals(Half left, Half right)
+        {
+            bool result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left == right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public bool HalfNotEquals(Half left, Half right) => left != right;
+        public bool HalfNotEquals(Half left, Half right)
+        {
+            bool result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left != right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public bool HalfLessThan(Half left, Half right) => left < right;
+        public bool HalfLessThan(Half left, Half right)
+        {
+            bool result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left < right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public bool HalfLessThanOrEqual(Half left, Half right) => left <= right;
+        public bool HalfLessThanOrEqual(Half left, Half right)
+        {
+            bool result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left <= right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public bool HalfGreaterThan(Half left, Half right) => left > right;
+        public bool HalfGreaterThan(Half left, Half right)
+        {
+            bool result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left > right;
+            }
+            return result;
+        }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = LoopCount)]
         [ArgumentsSource(nameof(Values))]
-        public bool HalfGreaterThanOrEqual(Half left, Half right) => left >= right;
+        public bool HalfGreaterThanOrEqual(Half left, Half right)
+        {
+            bool result = default;
+            for (int i = 0; i < LoopCount; i++)
+            {
+                result = left >= right;
+            }
+            return result;
+        }
     }
 }
